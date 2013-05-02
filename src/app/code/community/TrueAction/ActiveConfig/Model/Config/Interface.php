e4ab879706eae5fdf5841b6a51ac7eb23f9d3e09
Michael Phang <phangm@trueaction.com>
2013-05-02 11:13:43 -0400

    renamed the config generator interface.

--- src/app/code/community/TrueAction/ActiveConfig/Model/Config/Interface.php
+++ src/app/code/community/TrueAction/ActiveConfig/Model/Config/Interface.php
@@ -5,7 +5,8 @@ TrueAction_ActiveConfig_Model_Generator
 
 Interface class for all configuration generators
  */
-class TrueAction_ActiveConfig_Model_Generator {
+interface class TrueAction_ActiveConfig_Model_Config_Interface {
+
 	/**
 	 * generates the xml nodes that comprise a set of configuration fields
 	 * in the magento system config
