4c5885bb729df73750a4ffb81979d0adff6f0082
Michael Phang <phangm@trueaction.com>
2013-05-02 11:24:20 -0400

    fixed syntax error in the config interface.

--- src/app/code/community/TrueAction/ActiveConfig/Model/Config/Interface.php
+++ src/app/code/community/TrueAction/ActiveConfig/Model/Config/Interface.php
@@ -5,7 +5,7 @@ TrueAction_ActiveConfig_Model_Generator
 
 Interface class for all configuration generators
  */
-interface class TrueAction_ActiveConfig_Model_Config_Interface {
+interface TrueAction_ActiveConfig_Model_Config_Interface {
 
 	/**
 	 * generates the xml nodes that comprise a set of configuration fields
