dcc2cc495e962e1fa12548fe0e6215eafb6c7895
Michael Phang <phangm@trueaction.com>
2013-05-02 12:44:08 -0400

    fleshed out the ftp config generator.

--- src/app/code/community/TrueAction/FileTransfer/Test/Model/Config/Ftp.php
+++ src/app/code/community/TrueAction/FileTransfer/Test/Model/Config/Ftp.php
@@ -1,8 +1,8 @@
 <?php
 /*
-TrueAction_ActiveConfig_Model_Generator
+TrueAction_FileTransfer_Test_Model_Config_Ftp
 
-Interface class for all configuration generators
+tests for the ftp config generator.
  */
 class TrueAction_FileTransfer_Test_Model_Config_Ftp
 	extends EcomDev_PHPUnit_Test_Case
@@ -18,32 +18,22 @@ class TrueAction_FileTransfer_Test_Model_Config_Ftp
 	 * @test
 	 * */
 	public function testGetConfig() {
-		$model = new TrueAction_FileTransfer_Model_Config_Ftp();
+		$model = $this->class->newInstance();
 		$importOptions = null;
 		$config = $model->getConfig($importOptions);
 		$this->assertInstanceOf('Varien_Simplexml_Config', $config);
-		$this->assertTrue($config->hasChildren());
-
-		$fieldsXml = '
-			<fields>
-			<ftp_username translate="label">
-				<label>Username</label>
-				<frontend_type>text</frontend_type>
-			</ftp_username>
-			<ftp_password translate="label">
-				<label>Password</label>
-				<frontend_type>obscure</frontend_type>
-				<backend_model>adminhtml/system_config_backend_encrypted</backend_model>
-			</ftp_password>
-			<ftp_host translate="label">
-				<label>Remote Host</label>
-				<frontend_type>text</frontend_type>
-			</ftp_host>
-			<ftp_port translate="label">
-				<label>Remote Port</label>
-				<frontend_type>text</frontend_type>
-			</ftp_port>
-			</fields>
-		';
+		$node = $config->getNode();
+		$this->assertInstanceOf('Varien_Simplexml_Element', $node);
+		$this->assertTrue($node->hasChildren());
+		$names = array();
+		foreach ($node as $name => $node) {
+			$this->assertInstanceOf('Varien_Simplexml_Element', $node);
+			$names[] = $name;
+		}
+		$this->assertContains('filetransfer_ftp_username', $names);
+		$this->assertContains('filetransfer_ftp_password', $names);
+		$this->assertContains('filetransfer_ftp_host', $names);
+		$this->assertContains('filetransfer_ftp_port', $names);
+		$this->assertContains('filetransfer_ftp_remote_path', $names);
 	}
 }
