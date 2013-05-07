7bd06bd606b4df6a13bd2c2e6ed852b7c12d0a97
Michael Phang <phangm@trueaction.com>
2013-05-07 11:08:13 -0400

    refactored the code that reads the config out to the observer so the injector now only handles the insertion of the configuration.

--- src/app/code/community/TrueAction/ActiveConfig/Model/Fieldinjector.php
+++ src/app/code/community/TrueAction/ActiveConfig/Model/Fieldinjector.php
@@ -2,23 +2,12 @@
 /**
  * handles inserting the external system config xml nodes.
  * */
-class TrueAction_ActiveConfig_Model_FieldInjector extends Mage_Core_Model_Abstract
+class TrueAction_ActiveConfig_Model_FieldInjector
 {
-
-	const HANDLER_TAG = 'activeconfig_handler';
-
-	// the path to the placeholder nodes relative to a group node
-	// string
-	private $_importNodeName = 'activeconfig_import';
-
-	// the import setting nodes to be replaced by the new config.
-	// Varien_Simplexml_Element
-	private $_importNode = null;
-
 	// the fields node that will become the parent of the newly generated
 	// config nodes.
 	// Varien_Simplexml_Element
-	private $_fieldsCfg = null;
+	private $_fieldsConfig = null;
 
 	public function __construct()
 	{
@@ -27,87 +16,12 @@ class TrueAction_ActiveConfig_Model_FieldInjector extends Mage_Core_Model_Abstra
 		$this->_fieldsCfg->loadString('<fields/>');
 	}
 
-
 	/**
-	 * expectes config to be of the following structure
-	 *
-	 * <config><sections>...<groups>...<fields>
-     *  <activeconfig_import> <!--signals that an import is necessary -->
-     *    <module>            <!--module whose feature config we want to add-->
-     *      <feature/>        <!--feature whose config we're importing -->
-     *      <feature2>
-     *         ...            <!--config related to actual import-->
-     *      </feature2>
-     *    </module>
-     *    ...
-     *  </activeconfig_import>
-	 *
-	 * @param Varien_Simplexml_Element $importNode
+	 * inserts the field configuration nodes into the attached fieldset
+	 * @param Varien_Simplexml_Config $fieldsConfig
 	 * */
-	private function _readImportConfig($importNode)
+	public function insertConfig($fieldsConfig)
 	{
-		$this->_importNode = $importNode;
-		foreach ($importNode->children() as $moduleName => $moduleNode) {
-			foreach ($moduleNode->children() as $feature => $featureNode) {
-				$generator = $this->_getConfigGenerator($moduleName, $feature);
-				$config    = $generator->getConfig($featureNode);
-				$this->_fieldsCfg->extend($config);
-			}
-		}
-		return $this;
-	}
-
-	/**
-	 * returns a concrete injection class who is responsible for inserting
-	 * new nodes into the system config structure.
-	 *
-	 * reads the followig config structure:
-	 *
-	 * <config>
-	 *  <activeconfig_handler>
-	 *   <module>
-	 *    <feature>module/module:method</feature>
-	 *    ...
-	 *   </module>
-	 *   </module>
-	 *  </activeconfig_handler>
-	 * </config>
-	 *
-	 * @param string $module
-	 * @return TrueAction_ActiveConfig_Model_Config_Abstract
-	 * */
-	private function _getConfigGenerator($module, $feature)
-	{
-		$model = null;
-		$generatorNode = Mage::getConfig()->getNode(
-			self::HANDLER_TAG . '/' . $module  . '/' .  $feature
-		);
-		if ($generatorNode) {
-			$model = new TrueAction_FileTransfer_Model_Config_Ftp();
-			// $model = Mage::getModel($generatorNode->innerXml());
-		}
-		if (!$model) {
-			$model = new TrueAction_ActiveConfig_Model_Config_Abstract();
-		}
-		return $model;
-	}
-
-	/**
-	 * searches for placeholder nodes and replaces them with the specified
-	 * configuration nodes.
-	 * @param Varien_Simplexml_Element
-	 * */
-	public function processGroup($group)
-	{
-		Mage::log("group before:\n" . print_r($group, true));
-		$fieldNodes = $group->fields->children();
-		foreach ($fieldNodes as $fieldName => $fieldNode) {
-        	if ($fieldName === $this->_importNodeName) {
-        		$this->_readImportConfig($fieldNode);
-	        	$group->fields->extend($this->_fieldsCfg->getNode());
-        	}
-        }
-		Mage::log("group after:\n" . print_r($group, true));
-		Mage::log("group after:\n" . print_r($group->getParent(), true));
+    	$this->_fieldsConfig->extend($fieldsConfig->getNode());
 	}
 }
--- src/app/code/community/TrueAction/ActiveConfig/Model/Observer.php
+++ src/app/code/community/TrueAction/ActiveConfig/Model/Observer.php
@@ -2,7 +2,108 @@
 class TrueAction_ActiveConfig_Model_Observer
 {
 	private $_importsXpath = '';
+	private $_importSpecNode = 'fields/activeconfig_import';
 
+
+	const HANDLER_TAG = 'activeconfig_handler';
+
+	// format of the event name:
+	// activeconfig_<module_config_section>_<featurename>
+	const EVENT_FORMAT = 'activeconfig_%s_%s';
+
+	// the path to the placeholder nodes relative to a group node
+	// string
+	const IMPORT_NODE = 'activeconfig_import';
+
+	// the import setting nodes to be replaced by the new config.
+	// Varien_Simplexml_Element
+	private $_importNode = null;
+
+	// the fields node that will become the parent of the newly generated
+	// config nodes.
+	// Varien_Simplexml_Element
+	private $_fieldsConfig = null;
+
+	// the event to fire to insert the configuration
+	private $_eventName = '';
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->_fieldsCfg = new Varien_Simplexml_Config();
+		$this->_fieldsCfg->loadString('<fields/>');
+	}
+
+	/**
+	 * expectes config to be of the following structure
+	 *
+	 * <config><sections>...<groups>...<fields>
+     *  <activeconfig_import> <!--signals that an import is necessary -->
+     *    <module>            <!--module whose feature config we want to add-->
+     *      <feature/>        <!--feature whose config we're importing -->
+     *      <feature2>
+     *         ...            <!--config related to actual import-->
+     *      </feature2>
+     *    </module>
+     *    ...
+     *  </activeconfig_import>
+	 *
+	 * @param Varien_Simplexml_Element $importNode
+	 * */
+	private function _readImportConfig($importNode)
+	{
+		$this->_importNode = $importNode;
+		foreach ($importNode->children() as $moduleName => $moduleNode) {
+			foreach ($moduleNode->children() as $feature => $featureNode) {
+				$generator = $this->_generateEventName($moduleName, $feature);
+				$config    = $generator->getConfig($featureNode);
+				$this->_fieldsCfg->extend($config);
+			}
+		}
+		return $this;
+	}
+
+	/**
+	 * returns a concrete injection class who is responsible for inserting
+	 * new nodes into the system config structure.
+	 *
+	 * reads the followig config structure:
+	 *
+	 * <config>
+	 *  <activeconfig_handler>
+	 *   <module>
+	 *    <feature>module/module:method</feature>
+	 *    ...
+	 *   </module>
+	 *   </module>
+	 *  </activeconfig_handler>
+	 * </config>
+	 *
+	 * @param string $module
+	 * @return TrueAction_ActiveConfig_Model_Config_Abstract
+	 * */
+	private function _getConfigGenerator($module, $feature)
+	{
+		$model = null;
+		$generatorNode = Mage::getConfig()->getNode(
+			self::HANDLER_TAG . '/' . $module  . '/' .  $feature
+		);
+		if ($generatorNode) {
+			$model = new TrueAction_FileTransfer_Model_Config_Ftp();
+			// $model = Mage::getModel($generatorNode->innerXml());
+		}
+		if (!$model) {
+			$model = new TrueAction_ActiveConfig_Model_Config_Abstract();
+		}
+		return $model;
+	}
+
+	/**
+	 * this function is run only once after all the system.xml files have been
+	 * loaded. it scans each group for the presence of an import specification.
+	 * it then uses an injector model to get and insert the proper configuration
+	 * as defined in the import specifications.
+	 * */
 	public function processConfigImports($observer)
 	{
 		$config = $observer->getEvent()->getConfig();
