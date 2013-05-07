740c65352f9881363516c17972b64987b445eb4e
Michael Phang <phangm@trueaction.com>
2013-05-07 10:59:10 -0400

    added some hardcoded sort_order tags so that the config is in the proper order.

--- src/app/code/community/TrueAction/FileTransfer/Model/Config/Ftp.php
+++ src/app/code/community/TrueAction/FileTransfer/Model/Config/Ftp.php
@@ -36,6 +36,7 @@ class TrueAction_FileTransfer_Model_Config_Ftp
 			<{$this->_fieldPrefix}_username translate=\"label\">
 				<label>Username</label>
 				<frontend_type>text</frontend_type>
+				<sort_order>100</sort_order>
 				<show_in_default>1</show_in_default>
 				<show_in_website>1</show_in_website>
 				<show_in_store>1</show_in_store>
@@ -44,6 +45,7 @@ class TrueAction_FileTransfer_Model_Config_Ftp
 				<label>Password</label>
 				<frontend_type>obscure</frontend_type>
 				<backend_model>adminhtml/system_config_backend_encrypted</backend_model>
+				<sort_order>101</sort_order>
 				<show_in_default>1</show_in_default>
 				<show_in_website>1</show_in_website>
 				<show_in_store>1</show_in_store>
@@ -51,6 +53,7 @@ class TrueAction_FileTransfer_Model_Config_Ftp
 			<{$this->_fieldPrefix}_host translate=\"label\">
 				<label>Remote Host</label>
 				<frontend_type>text</frontend_type>
+				<sort_order>102</sort_order>
 				<show_in_default>1</show_in_default>
 				<show_in_website>1</show_in_website>
 				<show_in_store>1</show_in_store>
@@ -58,6 +61,7 @@ class TrueAction_FileTransfer_Model_Config_Ftp
 			<{$this->_fieldPrefix}_port translate=\"label\">
 				<label>Remote Port</label>
 				<frontend_type>text</frontend_type>
+				<sort_order>103</sort_order>
 				<show_in_default>1</show_in_default>
 				<show_in_website>1</show_in_website>
 				<show_in_store>1</show_in_store>
@@ -65,6 +69,7 @@ class TrueAction_FileTransfer_Model_Config_Ftp
 			<{$this->_fieldPrefix}_remote_path translate=\"label\">
 				<label>Remote Path</label>
 				<frontend_type>text</frontend_type>
+				<sort_order>104</sort_order>
 				<show_in_default>1</show_in_default>
 				<show_in_website>1</show_in_website>
 				<show_in_store>1</show_in_store>
