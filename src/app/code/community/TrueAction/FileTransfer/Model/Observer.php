c296851366cbaf9a5290a9eb8055d0ee32d21a7b
Michael Phang <phangm@trueaction.com>
2013-05-08 19:49:38 -0400

    changed the filetransfer observer to attempt to get the protocol model, from which, the config can be used to generate the fields.

--- src/app/code/community/TrueAction/FileTransfer/Model/Observer.php
+++ src/app/code/community/TrueAction/FileTransfer/Model/Observer.php
@@ -1,12 +1,15 @@
 <?php
 class TrueAction_FileTransfer_Model_Observer
 {
-	public function handleFtpConfigImport($observer)
+	public function handleConfigImport($observer)
 	{
 		$event = $observer->getEvent();
 		$injector = $event->getInjector();
-		$fields = Mage::getModel('filetransfer/protocol_config')
-			->generateFields($event->getConfig());
+		$config = Mage::helper('filetransfer')
+			->getProtocolModel($event->getConfigPath())
+			->getConfig();
+		$fields = $config->generateFields($event->getModuleSpec());
+		Mage::log(print_r($fields, true));
 		$injector->insertConfig($fields);
 	}
 }
