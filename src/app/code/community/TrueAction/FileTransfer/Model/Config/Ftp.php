929f472409fb3690ae5e5e8782cfbc4ad451fc22
Michael Phang <phangm@trueaction.com>
2013-05-07 22:40:23 -0400

    added a protocol selection field (just a plain text field right now). removed the constructor.

--- src/app/code/community/TrueAction/FileTransfer/Model/Config/Ftp.php
+++ src/app/code/community/TrueAction/FileTransfer/Model/Config/Ftp.php
@@ -7,12 +7,7 @@ concrete configuration generator for the ftp protocol.
 class TrueAction_FileTransfer_Model_Config_Ftp
 	extends TrueAction_ActiveConfig_Model_Config_Abstract
 {
-	private $_fieldPrefix = '';
-
-	public function __construct()
-	{
-		$this->_fieldPrefix = sprintf('%s_%s', 'filetransfer', 'ftp');
-	}
+	private $_fieldPrefix = 'filetransfer_ftp';
 
 	/**
 	 * returns the field prefix for each field
@@ -33,6 +28,14 @@ class TrueAction_FileTransfer_Model_Config_Ftp
 		$fields = new Varien_Simplexml_Config();
 		$fields->loadString("
 			<fields>
+			<ftransfer_protocol translate=\"label\">
+				<label>Protocol</label>
+				<frontend_type>text</frontend_type>
+				<sort_order>99</sort_order>
+				<show_in_default>1</show_in_default>
+				<show_in_website>1</show_in_website>
+				<show_in_store>1</show_in_store>
+			</ftransfer_protocol>
 			<{$this->_fieldPrefix}_username translate=\"label\">
 				<label>Username</label>
 				<frontend_type>text</frontend_type>
