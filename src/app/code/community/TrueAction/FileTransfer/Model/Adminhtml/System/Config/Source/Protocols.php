00d2995906231ec14923a60c826b7b99d16a7173
Michael Phang <phangm@trueaction.com>
2013-05-08 20:34:02 -0400

    changed the protocol source model to use a string as the value.

--- src/app/code/community/TrueAction/FileTransfer/Model/Adminhtml/System/Config/Source/Protocols.php
+++ src/app/code/community/TrueAction/FileTransfer/Model/Adminhtml/System/Config/Source/Protocols.php
@@ -4,16 +4,16 @@ class TrueAction_FileTransfer_Model_Adminhtml_System_Config_Source_Protocols
 	public function toOptionArray()
 	{
 		return array(
-			array("value"=>0, "label"=>Mage::helper('filetransfer')->__("FTP")),
-			array("value"=>1, "label"=>Mage::helper('filetransfer')->__("SFTP")),
+			array("value"=>"ftp",  "label"=>Mage::helper('filetransfer')->__("FTP")),
+			array("value"=>"sftp", "label"=>Mage::helper('filetransfer')->__("SFTP")),
 		);
 	}
 
 	public function toArray()
 	{
 		return array(
-			0 => Mage::helper('filetransfer')->__("FTP"),
-			1 => Mage::helper('filetransfer')->__("SFTP"),
+			"ftp"  => Mage::helper('filetransfer')->__("FTP"),
+			"sftp" => Mage::helper('filetransfer')->__("SFTP"),
 		);
 	}
 }
