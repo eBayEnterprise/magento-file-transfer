eedeef1a17239cbe54d3f7b77cdcef6ba17373f5
Michael Phang <phangm@trueaction.com>
2013-05-16 17:30:07 -0400

    renamed sftp to ftps.

--- src/app/code/community/TrueAction/FileTransfer/Model/Protocol/Types/Ftps.php
+++ src/app/code/community/TrueAction/FileTransfer/Model/Protocol/Types/Ftps.php
@@ -1,24 +1,24 @@
 <?php
 /*
-TrueAction_FileTransfer_Model_Protocol_Types_Sftp
+TrueAction_FileTransfer_Model_Protocol_Types_ftps
 
 concrete configuration generator for the ftp protocol.
  */
-class TrueAction_FileTransfer_Model_Protocol_Types_Sftp extends TrueAction_FileTransfer_Model_Protocol_Types_Ftp
+class TrueAction_FileTransfer_Model_Protocol_Types_Ftps extends TrueAction_FileTransfer_Model_Protocol_Types_Ftp
 {
 	protected $_fieldMap = array(
-		'filetransfer_sftp_username'    => 'username',
-		'filetransfer_sftp_password'    => 'password',
-		'filetransfer_sftp_host'        => 'host',
-		'filetransfer_sftp_port'        => 'port',
-		'filetransfer_sftp_remote_path' => 'remote_path',
+		'filetransfer_ftps_username'    => 'username',
+		'filetransfer_ftps_password'    => 'password',
+		'filetransfer_ftps_host'        => 'host',
+		'filetransfer_ftps_port'        => 'port',
+		'filetransfer_ftps_remote_path' => 'remote_path',
 	);
 
 
 	public function _construct()
 	{
 		$this->setName('File Transfer Protocol (SSL)');
-		$this->setCode('sftp');
+		$this->setCode('ftps');
 		// create magic getter/setters for each field
 		$this->getConfig()->loadMappedFields($this->_fieldMap);
 	}
@@ -36,7 +36,7 @@ class TrueAction_FileTransfer_Model_Protocol_Types_Sftp extends TrueAction_FileT
 		$this->_conn = ftp_ssl_connect($config->getHost(), $config->getPort());
 		if (!$this->_conn){
 			try{
-				Mage::throwException("Failed to connect to 'ftp://$this->_host'.");
+				Mage::throwException("Failed to connect to 'ftps://$this->_host'.");
 			} catch (Exception $e) {
 				$success = false;
 				Mage::logException($e);
