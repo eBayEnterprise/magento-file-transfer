ad7a2b6156cb0bd4a4e7151a3698a7e1be6598a7
Michael Phang <phangm@trueaction.com>
2013-05-16 13:53:21 -0400

    renamed the get file test function and added a send file test function.

--- src/app/code/community/TrueAction/FileTransfer/Test/Connecttests.php
+++ src/app/code/community/TrueAction/FileTransfer/Test/Connecttests.php
@@ -5,8 +5,21 @@ class TrueAction_FileTransfer_Test_ConnectTests extends EcomDev_PHPUnit_Test_Cas
 	 * @test
 	 * @loadFixture getfile
 	 * */
-	public function testConnectivity() {
+	public function testGetFile() {
 		$result = Mage::helper('filetransfer')->getFile(
+			'ubuntu-archive-keyring.gpg',
+			'/tmp/foo.txt',
+			'testsection/testgroup'
+		);
+		$this->assertTrue($result);
+	}
+
+	/**
+	 * @test
+	 * @loadFixture sendfile
+	 * */
+	public function testSendFile() {
+		$result = Mage::helper('filetransfer')->sendFile(
 			'ubuntu-archive-keyring.gpg',
 			'/tmp/foo.txt',
 			'testsection/testgroup'
