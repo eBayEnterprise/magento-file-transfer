fad1935c463b56661ac1f6eb249c5cc6f4595684
Michael Phang <phangm@trueaction.com>
2013-06-07 16:49:50 -0400

    added tests for the getProtocolModel helper function.

---
+++ src/app/code/community/TrueAction/FileTransfer/Test/Helper/DataTests.php
@@ -0,0 +1,41 @@
+<?php
+class TrueAction_ActiveConfig_Test_Helper_DataTests extends EcomDev_PHPUnit_Test_Case_Config
+{
+	/**
+	 * @loadFixture config.yaml
+	 */
+	public function testGetProtocolModel()
+	{
+		$model = Mage::helper('filetransfer')
+			->getProtocolModel('testsection/testgroup');
+		$this->assertSame(
+			'TrueAction_FileTransfer_Model_Protocol_Types_Ftp',
+			get_class($model)
+		);
+	}
+
+	/**
+	 * @loadFixture config.yaml
+	 */
+	public function testGetProtocolModelWithProtocol()
+	{
+		$model = Mage::helper('filetransfer')
+			->getProtocolModel('testsection/testgroup', 'ftp');
+		$this->assertSame(
+			'TrueAction_FileTransfer_Model_Protocol_Types_Ftp',
+			get_class($model)
+		);
+		$model = Mage::helper('filetransfer')
+			->getProtocolModel('testsection/testgroup', 'ftps');
+		$this->assertSame(
+			'TrueAction_FileTransfer_Model_Protocol_Types_Ftps',
+			get_class($model)
+		);
+		$model = Mage::helper('filetransfer')
+			->getProtocolModel('testsection/testgroup', 'sftp');
+		$this->assertSame(
+			'TrueAction_FileTransfer_Model_Protocol_Types_Sftp',
+			get_class($model)
+		);
+	}
+}
