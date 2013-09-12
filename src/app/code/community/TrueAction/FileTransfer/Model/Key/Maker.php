93d107961734de691a9c86fd02be6368b42192f1
Michael A. Smith <msmith3@ebay.com>
2013-09-11 23:34:58 -0400

    Proposal for making ssh keys a bit more secure until we have a better solution.

--- src/app/code/community/TrueAction/FileTransfer/Model/Key/Maker.php
+++ src/app/code/community/TrueAction/FileTransfer/Model/Key/Maker.php
@@ -1,8 +1,8 @@
 <?php
-
-class TrueAction_FileTransfer_Model_Key_Maker
-	extends Varien_Object
+class TrueAction_FileTransfer_Model_Key_Maker extends Varien_Object
 {
+	private $_publicKeyPath;
+	private $_privateKeyPath;
 
 	/**
 	 * Varien_Object constructor - set up FsTool and base dirs
@@ -13,10 +13,40 @@ class TrueAction_FileTransfer_Model_Key_Maker
 			$this->setFsTool(new Varien_Io_File());
 		}
 		if (!$this->hasBaseDir()) {
-			$this->setBaseDir(
-				$this->getFsTool()->getCleanPath(Mage::getBaseDir('tmp') . DS . uniqid() . DS)
-			);
+			$this->setBaseDir(Mage::getBaseDir('tmp'));
 		}
+		if (!$this->hasTmpPrefix()) {
+			// Advise callers to set a prefix value for the tempnam() builtin.
+			// It makes debugging easier, at least.
+			Mage::log(
+				sprintf('[ %s ] prefix not set for tempfile. Using classname as generic default.', __CLASS__),
+				Zend_Log::WARN
+			);
+			$this->setTmpPrefix(__CLASS__);
+		}
+		// Don't make it possible for calling functions to modify the public
+		// and private key paths. They should always be created by a secure
+		// temporary file creation tool.
+		$this->_publicKeyPath = tempnam($this->getBaseDir(), $this->getTmpPrefix());
+		$this->_privateKeyPath = tempnam($this->getBaseDir(), $this->getTmpPrefix());
+	}
+
+	/**
+	 * Get the public key path
+	 * @return string file path to the public key
+	 */
+	public function getPublicKeyPath()
+	{
+		return $this->_publicKeyPath;
+	}
+
+	/**
+	 * Get the private key path
+	 * @return string file path to the private key
+	 */
+	public function getPrivateKeyPath()
+	{
+		return $this->_privateKeyPath;
 	}
 
 	/**
@@ -24,50 +54,21 @@ class TrueAction_FileTransfer_Model_Key_Maker
 	 */
 	public function __destruct()
 	{
-		$this->destroyKeys();
-	}
-
-	/**
-	 * If the base dir for the keys does not exist, create it.
-	 * @return  boolean If the file exists.
-	 */
-	protected function _setUpKeyDir()
-	{
-		return $this->getFsTool()->checkAndCreateFolder($this->getBaseDir(), 0644);
-	}
-
-	/**
-	 * Path to the public key. If a public key file name hasn't been created yet, generate a new one.
-	 * @return string
-	 */
-	public function getPublicKeyPath()
-	{
-		$this->_pubKeyName = $this->_pubKeyName ?: uniqid();
-		return $this->getBaseDir() . $this->_pubKeyName;
-	}
-
-	/**
-	 * Path to the private key. If a private key file name hasn't been created yet, generate a new one.
-	 * @return string
-	 */
-	public function getPrivateKeyPath()
-	{
-		$this->_privKeyName = $this->_privKeyName ?: uniqid();
-		return $this->getBaseDir() . $this->_privKeyName;
+		$this->_destroyKeys();
 	}
 
 	/**
 	 * Create a key with the given string as contents
-	 * @param  string $keyContents contents of the key file
-	 * @return boolean             successfully created keys
+	 * @param string $pubKey contents to put in the public key file
+	 * @param string $privKey contents to put in the private key file
+	 * @return boolean successfully created keys
 	 */
 	public function createKeyFiles($pubKey, $privKey)
 	{
-		$this->_setUpKeyDir();
 		// this is required by Varien_File_Io - sets up the _iw property,
 		// without which the write calls error out. >:|
 		$this->getFsTool()->open(array('path' => $this->getBaseDir()));
-		$pubCreated = $this->getFsTool()->write($this->getPublicKeyPath(), $pubKey, 0644);
+		$pubCreated = $this->getFsTool()->write($this->getPublicKeyPath(), $pubKey, 0600);
 		$privCreated = $this->getFsTool()->write($this->getPrivateKeyPath(), $privKey, 0600);
 		$msg = sprintf(
 			'[ %s ] Generated files successfully: pub - %d, priv - %d', __CLASS__,
@@ -78,23 +79,27 @@ class TrueAction_FileTransfer_Model_Key_Maker
 	}
 
 	/**
-	 * Destory/delete the given key file
-	 * @param  sting    $keyLocation path to the file to delete
-	 * @return boolean
+	 * Destroy/delete the key files
+	 * @return null
 	 */
-	public function destroyKeys()
+	private function _destroyKeys()
 	{
-		$results = true;
-		if ($this->getFsTool()->fileExists($this->getBaseDir(), false)) {
-			$this->getFsTool()->cd($this->getBaseDir());
-			foreach ($this->getFsTool()->ls(Varien_Io_File::GREP_FILES) as $keyFile) {
-				$results = $results && $this->getFsTool()->rm($keyFile['text']);
-			}
-			$results = $results && $this->getFsTool()->rmdir($this->getBaseDir());
+		$fs = $this->getFsTool();
+		$pub = $this->getPublicKeyPath();
+		$priv = $this->getPrivateKeyPath();
+		if ($fs->fileExists($pub)) {
+			$fs->rm($pub);
+			Mage::log(
+				sprintf('[ %s ] Deleted public key "%s".', __CLASS__, $pub),
+				Zend_Log::DEBUG
+			);
 		}
-		$msg = sprintf('[ %s ] Files deleted successfully: %d', __CLASS__, $results);
-		Mage::log($msg, Zend_Log::DEBUG);
-		return $results;
+		if ($fs->fileExists($priv)) {
+			$fs->rm($priv);
+			Mage::log(
+				sprintf('[ %s ] Deleted private key "%s".', __CLASS__, $priv),
+				Zend_Log::DEBUG
+			);
+		}
 	}
-
 }
